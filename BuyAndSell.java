d3b9525470eed3915121de18efe25bbdc56227e2
sudeepcodeforFun <150527984+sudeepcodeforFun@users.noreply.github.com>
2023-12-28 17:00:24 +0530

    Buy and sell Stock Problem(solution)

---
+++ BuyAndSell.java
@@ -0,0 +1,22 @@
+public class BuyAndSell {
+
+    public static int buyAndSellstock(int prices[]){
+        int buyprice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<prices.length; i++){
+            if(buyprice<prices[i]){
+                int profit = prices[i] - buyprice;
+                maxProfit = Math.max(maxProfit, profit);
+            }else{
+                buyprice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int price [] = {7,1,5,3,6,4};
+        System.out.println(buyAndSellstock(price));
+
+    }
+}
