e5a5b526e46f197b70d17d335cd584aa244277ec
sudeepcodeforFun <150527984+sudeepcodeforFun@users.noreply.github.com>
2024-01-26 18:25:20 +0530

    X to power N
    This is the code is little tricky for the negative power otherwise it's easy.

---
+++ PrintxtoPowern.java
@@ -0,0 +1,32 @@
+public class PrintxtoPowern {
+    
+    // public static int powerN(int n, int power){
+    //     if(power==1){
+    //         return n;
+    //     }
+    //     return n*powerN(n, power-1);
+    // }
+
+    public static double powerN(double n, int power){
+        if(power == 0){
+            return 1;
+        }
+        double halfPower = powerN(n, power/2);
+        if(power%2 == 0){
+            return halfPower * halfPower;
+        }
+        else{
+            if(power>0){
+                return n * halfPower * halfPower; 
+            }
+            else{
+                return (halfPower * halfPower)/n;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        double n = 2.0000;
+        int power = -2;
+        System.out.println(powerN(n, power));
+    }
+}
