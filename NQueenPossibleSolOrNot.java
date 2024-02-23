3c98e7eff205414336a8f4636cbc14e25a9abf3d
sudeepcodeforFun <150527984+sudeepcodeforFun@users.noreply.github.com>
2024-02-23 17:37:27 +0530

    N-Queen either possible Solution or Not
    This demonstrates whether the possible solution exists or not if yes then print the first solution.

---
+++ NQueenPossibleSolOrNot.java
@@ -0,0 +1,65 @@
+public class NQueenPossibleSolOrNot {
+    public static boolean isSafe(char board[][], int row, int col){
+
+        for(int i= row-1; i>=0; i--){
+            if(board[i][col] == 'Q')
+                return false;
+        }
+
+        for(int i = row-1, j = col-1; i>=0 && j>=0; i--, j--){
+            if(board[i][j] == 'Q')
+                return false;
+        }
+
+        for(int i = row-1, j = col+1; i>=0 && j<board.length; i--, j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean nQueen(char board[][], int row){
+
+        if(row == board.length){
+            return true;
+        }
+        for(int j=0; j<board.length; j++){
+            if(isSafe(board, row, j)){
+
+                board[row][j] = 'Q';
+                if(nQueen(board, row+1)){
+                    return true;
+                }
+                board[row][j] = 'X';   // erasing or you can backtracking
+            }
+        }
+        return false;
+    }
+    public static void display(char board[][]){
+        System.out.println("-------chess Board-------");
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board.length; j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        char board[][] = new char[n][n];
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board.length; j++){
+                board[i][j] = 'X';
+            }
+        }
+        if(nQueen(board,0)){
+            System.out.println("Solution is possible");
+            display(board);
+        }else{
+            System.out.println("Soltion is not possible");
+        }
+
+    }
+}
