ff19c6ae6a53f7a195b2e01cc8d8357df1cef83a
sudeepcodeforFun <150527984+sudeepcodeforFun@users.noreply.github.com>
2024-04-27 22:50:09 +0530

    Reverse String using Stack
    Easy Level...

---
+++ StackReverseString.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class StackReverseString {
+
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int idx = 0;
+        while (idx<str.length()) {
+            s.push(str.charAt(idx));
+            idx++;
+        }
+        StringBuilder res = new StringBuilder("");
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            res.append(curr);
+        }
+
+        return res.toString();
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        System.out.println(reverseString(str));
+        
+    }
+}
