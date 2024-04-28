a1c8ad309912b0920a7dbdabc714849eadd2155f
sudeepcodeforFun <150527984+sudeepcodeforFun@users.noreply.github.com>
2024-04-28 13:44:10 +0530

    Stock Span Problem
    A classical problem where we need to calculate the span can vary in various forms. it is the one form where span = price<= today's price.

---
+++ StackStockSpan.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class StackStockSpan {
+
+    public static void calSpanStock(int stock[], int span[]){
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        for(int i=1; i<stock.length; i++){
+            while (!s.isEmpty() && stock[i]>=stock[s.peek()]) {
+                s.pop();
+            }
+            if(s.isEmpty()){
+                span[i] = i+1;
+            }
+            else{
+                span[i] = i-s.peek();
+            }
+            s.push(i);
+        }
+    }
+    public static void main(String[]args){
+        int stock[] = {100,80,60,70,60,85,100};
+        int span[] = new int[7];
+        calSpanStock(stock, span);
+        for(int i =0; i<span.length; i++){
+            System.out.println(span[i]);
+        }
+    }
+}
